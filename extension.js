22d774f82f9e36c01fcf972e18a90ee72ad911eb
Joe Strong <joestrong@gmail.com>
2015-03-05 21:18:20 +0000

    Tidied up defaults

--- extension.js
+++ extension.js
@@ -5,40 +5,19 @@ const Tweener = imports.ui.tweener;
 
 let text, button;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
-
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
+    button = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+    let icon = new St.Icon({
+        icon_name: 'system-run-symbolic',
+        style_class: 'system-status-icon'
+    });
 
     button.set_child(icon);
     button.connect('button-press-event', _showHello);
@@ -50,4 +29,30 @@ function enable() {
 
 function disable() {
     Main.panel._rightBox.remove_child(button);
+}
+
+function _showHello() {
+    if (!text) {
+        text = new St.Label({ style_class: 'helloworld-label', text: "Git Flow" });
+        Main.uiGroup.add_actor(text);
+    }
+
+    text.opacity = 255;
+
+    let monitor = Main.layoutManager.primaryMonitor;
+
+    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
+                      Math.floor(monitor.height / 2 - text.height / 2));
+
+    Tweener.addTween(text, {
+        opacity: 0,
+        time: 2,
+        transition: 'easeOutQuad',
+        onComplete: _hideHello
+    });
+}
+
+function _hideHello() {
+    Main.uiGroup.remove_actor(text);
+    text = null;
 }
